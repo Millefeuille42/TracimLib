37cae5bba4e2bae0ccecf5b866b2023ddb04a9ac
millefeuille <mathieu.labourier@algoo.fr>
2022-12-27 11:08:30 +0100

    Create request helper

---
+++ utils.py
@@ -0,0 +1,9 @@
+import json
+
+
+def do_json_request(session, url, method, data={}):
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+    response = session.request(url=url, method=method, data=json.dumps(data), headers=headers)
+    if response.status_code != 200:
+        raise Exception({response.text, response.status_code})
+    return response
